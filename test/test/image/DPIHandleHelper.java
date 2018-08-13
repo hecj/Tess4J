93da1fb4d1c392c6cfb3dc814996ce9ec55b073e
hechaojie <25693780@qq.com>
2018-08-13 14:44:36 +0800

    up

---
+++ test/test/image/DPIHandleHelper.java
@@ -0,0 +1,46 @@
+package test.image;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import com.sun.image.codec.jpeg.JPEGCodec;
+import com.sun.image.codec.jpeg.JPEGEncodeParam;
+import com.sun.image.codec.jpeg.JPEGImageEncoder;
+ 
+public class DPIHandleHelper {
+    private static int DPI = 300;
+ 
+    public static void main(String[] args) {
+        String path = "/Users/hecj/Desktop/ocrdata/image/WechatIMG149.jpeg";
+        File file = new File(path);
+        handleDpi(file, DPI, DPI);
+    }
+ 
+    /**
+     * 改变图片DPI
+     *
+     * @param file
+     * @param xDensity
+     * @param yDensity
+     */
+    public static void handleDpi(File file, int xDensity, int yDensity) {
+        try {
+            BufferedImage image = ImageIO.read(file);
+            JPEGImageEncoder jpegEncoder = JPEGCodec.createJPEGEncoder(new FileOutputStream(file));
+            JPEGEncodeParam jpegEncodeParam = jpegEncoder.getDefaultJPEGEncodeParam(image);
+            jpegEncodeParam.setDensityUnit(JPEGEncodeParam.DENSITY_UNIT_DOTS_INCH);
+            jpegEncoder.setJPEGEncodeParam(jpegEncodeParam);
+            jpegEncodeParam.setQuality(0.75f, false);
+            jpegEncodeParam.setXDensity(xDensity);
+            jpegEncodeParam.setYDensity(yDensity);
+            jpegEncoder.encode(image, jpegEncodeParam);
+            image.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
